30f9c33f700322c38b891631cbe19f0f53819e00
Richard Fenix <richard.fenix@gmail.com>
2018-07-24 20:34:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,12 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 
+  
 end
